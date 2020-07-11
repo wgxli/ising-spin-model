2e8faf1b1262f02319df918b78fe7ac3254d804c
Samuel Li <git@samuelj.li>
2020-07-11 09:08:02 -0400

    Updates

---
+++ precache-manifest.72b3605e3e6bcdb700d4b7ca1db83aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81446beda661d0c17be71cf498869d17",
+    "url": "/ising-spin-model/index.html"
+  },
+  {
+    "revision": "d45bdf27dd5c2013019e",
+    "url": "/ising-spin-model/static/css/main.40c6684d.chunk.css"
+  },
+  {
+    "revision": "f72ea7b7df1718ad6d7b",
+    "url": "/ising-spin-model/static/js/2.007e0983.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ising-spin-model/static/js/2.007e0983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45bdf27dd5c2013019e",
+    "url": "/ising-spin-model/static/js/main.752b6350.chunk.js"
+  },
+  {
+    "revision": "dd5118501df41d18c908",
+    "url": "/ising-spin-model/static/js/runtime-main.171c5bd4.js"
+  }
+]);
